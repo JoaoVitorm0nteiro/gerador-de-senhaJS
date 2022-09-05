72663ea45b0b848d5608251a09f3da7950c95037
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-09-04 21:06:40 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const passwordgenerator = (num) =>{
+    const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    senha = ''
+    for(let i = 0; i<num; i++){
+        /* senha.concat(characters[random(0, characters.length-1)]) */
+        senha += characters[(random(0, characters.length-1))]
+    }
+    console.log(senha);
+}
+passwordgenerator(5)
