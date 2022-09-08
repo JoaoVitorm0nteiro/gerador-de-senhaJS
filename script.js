09ae7467299a65898cd182981f19ed6053e59d23
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-09-08 10:51:16 -0300

    commit 1, por enquanto a senha é retornada num log

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const passwordgenerator = (num) =>{
+const btn = document.querySelector("#send")
+btn.addEventListener("click", function (e) {
+    e.preventDefault()
+    const input = document.querySelector("#valor")
+    const formatedinpt = parseInt(input.value)
+    //console.log(typeof formatedinpt);
+    
+    const passwordgenerator = (formatedinpt) =>{
+        const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+    
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        senha = ''
+        for(let i = 0; i<formatedinpt; i++){
+            senha.concat(characters[random(0, characters.length-1)]) 
+            senha += characters[(random(0, characters.length-1))]
+        }
+        console.log(senha);
+    }
+    passwordgenerator(formatedinpt)
+})
+ 
+/* const passwordgenerator = (num) =>{
     const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
     characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     senha = ''
     for(let i = 0; i<num; i++){
-        /* senha.concat(characters[random(0, characters.length-1)]) */
+        /* senha.concat(characters[random(0, characters.length-1)]) 
         senha += characters[(random(0, characters.length-1))]
     }
     console.log(senha);
 }
-passwordgenerator(5)
+passwordgenerator(5) */
